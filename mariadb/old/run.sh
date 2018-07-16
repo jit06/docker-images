418e1974ea560abba405a42602dc6913d3e083fe
jit06 <jit@bluemind.org>
2018-07-16 22:19:09 +0200

    removed an obsolete dir

--- mariadb/old/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 <name> <port> <mysql conf volume> <mysql data volume>"
-    exit 1
-fi
-
-docker run --restart=always -d -p $1:3306 --name $2 -v $3:/etc/mysql -v $4:/var/lib/mysql mysql
