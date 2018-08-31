b99cacdd77c1c239af0c2875fcc1ff2aa40fd176
jit06 <jit@bluemind.org>
2018-08-31 23:39:37 +0200

    added first version of phpmyadmin

---
+++ phpmyadmin/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
+    cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
+<?php
+\$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
+EOT
+fi
+
+if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
+    touch /etc/phpmyadmin/config.user.inc.php
+fi
+
+mkfifo -m 600 /tmp/logpipehttp
+cat <> /tmp/logpipehttp 1>&2 &
+chown http /tmp/logpipehttp
+
+exec "$@"
