90fb28868d200db273d4cd6debe1ace9cc5d1e68
jit06 <jit@bluemind.org>
2018-09-03 23:18:43 +0200

    correct path to config files in /etc

--- phpmyadmin/run.sh
+++ phpmyadmin/run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
-    cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
+if [ ! -f /etc/webapps/phpmyadmin/config.secret.inc.php ]; then
+    cat > /etc/webapps/phpmyadmin/config.secret.inc.php <<EOT
 <?php
 \$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
 EOT
 fi
 
-if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
-    touch /etc/phpmyadmin/config.user.inc.php
+if [ ! -f /etc/webapps/phpmyadmin/config.user.inc.php ]; then
+    touch /etc/webapps/phpmyadmin/config.user.inc.php
 fi
 
 mkfifo -m 600 /tmp/logpipehttp
