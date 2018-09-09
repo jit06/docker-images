29165c75cdf2f746695d737315f63e6e4a457a88
jit06 <jit@bluemind.org>
2018-09-09 23:29:56 +0200

    changed path to backup files

--- bozon/run.sh
+++ bozon/run.sh
@@ -4,13 +4,13 @@
 if [ -f /mnt/bozon_conf_site_share.tar.gz ] ; then
     echo "found a backup for configuration, trying to restore..."
     cd /private
-    tar -zxf bozon_conf_site_share.tar.gz 
+    tar -zxf /mnt/bozon_conf_site_share.tar.gz
 fi
 
 if [ -f /mnt/bozon_data_site_share.tar.gz ] ; then
     echo "found a backup for data (uploads), trying to restore..."
     cd /uploads
-    tar -zxf bozon_data_site_share.tar.gz 
+    tar -zxf /mnt/bozon_data_site_share.tar.gz 
 fi
 
 
