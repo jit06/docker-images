22d231559e38e462067913c8dee1876604ad0d7d
jit06 <jit@bluemind.org>
2018-09-09 23:16:12 +0200

    add backup handling from tar.gz file in /mnt

--- bozon/run.sh
+++ bozon/run.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-# restore a previous backup (from mysqldump --all-databases) if any
-if [ -f /mnt/sql/all-databases.sql ] ; then
-    echo "found a backup, trying to restore..."
+# restore a previous backup (tar.gz archive) if any
+if [ -f /mnt/bozon_conf_site_share.tar.gz ] ; then
+    echo "found a backup for configuration, trying to restore..."
+    cd /private
+    tar -zxf bozon_conf_site_share.tar.gz 
+fi
+
+if [ -f /mnt/bozon_data_site_share.tar.gz ] ; then
+    echo "found a backup for data (uploads), trying to restore..."
+    cd /uploads
+    tar -zxf bozon_data_site_share.tar.gz 
 fi
 
 
