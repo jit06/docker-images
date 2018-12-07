1a26f0008a90f8b4ff5bc1799b482aa350945f41
jit06 <julien@bluemind.org>
2018-12-07 22:54:35 +0100

    some bug corrected

--- nginx_naxsi/nginx-entrypoint.sh
+++ nginx_naxsi/nginx-entrypoint.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
-if [ ! -f /opt/configured ]; then
-	echo "" > /etc/nginx/nginx.conf
-	echo "error_log stderr ;" >> /etc/nginx/nginx.conf
-
-	if [ "$WORKER_PROCESS" -ne 0 ] ; then
-		echo "worker_processes $WORKER_PROCESS ;" >> /etc/nginx/nginx.conf
-	fi
-	
-	if [ "$WORKER_CONNECTIONS" -ne 0 ] ; then
-		echo "events {" >> /etc/nginx/nginx.conf
-		echo "worker_connections $WORKER_CONNECTIONS ;" >> /etc/nginx/nginx.conf
-		echo "}" >> /etc/nginx/nginx.conf
-        fi
-
-	echo "include /etc/nginx/conf.d/base_conf;" >> /etc/nginx/nginx.conf
-	echo "include /etc/nginx/conf.d/reverse_conf/*;" >> /etc/nginx/nginx.conf
-	echo "daemon off;" >> /etc/nginx/nginx.conf
-
-	if [ -f /etc/nginx/conf.d/ssl_hosts ] ; then
-		certbot certonly --noninteractive --agree-tos --config /etc/nginx/conf.d/letsencrypt_init.ini
-	fi
-
-	touch /opt/configured
-fi
-
-
 # restore configuration from a backup if any
-if [ -f /mnt/$BACKUP_CONF_ARCHIVE ] && [ ! -f /etc/nginx/conf.d/restored ] ; then
+if [ -f /mnt/$BACKUP_CONF_ARCHIVE ] && [ ! -f /etc/nginx/restored ] ; then
     echo "found a backup for conf.d (/mnt/$BACKUP_CONF_ARCHIVE), trying to restore..."
-    cd /etc/nginx/conf.d
+    cd /etc/nginx
     tar -zxf /mnt/$BACKUP_CONF_ARCHIVE
-    chown -R http:http /etc/nginx/conf.d
-    touch /etc/nginx/conf.d/restored
+    chown -R http /etc/nginx
+    touch /etc/nginx/restored
 fi
 
 # restore configuration from a backup if any
@@ -43,15 +17,20 @@ if [ -f /mnt/$BACKUP_LE_ARCHIVE ] && [ ! -f /etc/letsencrypt/restored ] ; then
     touch /etc/letsencrypt/restored
 fi
 
-
-if [ -f /etc/nginx/conf.d/ssl_hosts ] ; then
-	/usr/local/bin/letsencrypt_renew.sh &
+if [ -f /etc/nginx/ssl_hosts ] ; then
+    certbot certonly --noninteractive --agree-tos --config /etc/nginx/letsencrypt_init.ini
+    /usr/local/bin/letsencrypt_renew.sh &
 fi
-
 
 if [ ! -d /var/tmp/nginx/temp ]; then
 	mkdir -p /var/tmp/nginx/temp
 fi
 
-echo "Launch nginx with /etc/nginx/nginx.conf"
-exec "$@ -c /etc/nginx/nginx.conf"
+# if command starts with an option, prepend /usr/bin/nginx
+if [ "${1:0:1}" = '-' ]; then
+	set -- /usr/bin/nginx "$@"
+fi
+
+echo "Launch nginx with given arguments:"
+echo "$@"
+exec "$@"
